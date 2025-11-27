c0f70922ee05c93160cfe2e83e0c622271fb45af
Andrey Ponomarev <info@andreyponomarev.ru>
2025-11-27 19:38:11 +0300

    Do not mutate Express obj props

--- api/src/custom.d.ts
+++ api/src/custom.d.ts
@@ -1,10 +1,15 @@
-import "jest-extended";
-
 declare global {
   namespace Express {
     interface Request {
       // Now in your code your can access it like 'req.tenant'
       tenant?: string;
+      validated?: {
+        params?: Record<string, unknown>;
+        query?: Record<string, unknown>;
+        body?: Record<string, unknown>;
+        headers?: Record<string, unknown>;
+        files?: Record<string, unknown>;
+      };
     }
   }
 }
