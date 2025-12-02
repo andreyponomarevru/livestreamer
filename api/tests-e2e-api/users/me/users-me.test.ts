be033de243852f00b7c9b3be6ffc452859ee6843
Andrey Ponomarev <info@andreyponomarev.ru>
2025-12-02 20:25:16 +0300

    Refactor

--- api/tests-e2e-api/users/me/users-me.test.ts
+++ api/tests-e2e-api/users/me/users-me.test.ts
@@ -1,83 +1,63 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, beforeAll, afterAll } from "@jest/globals";
 import request from "supertest";
 import { faker } from "@faker-js/faker";
 
 import { httpServer } from "../../../src/http-server";
-import { createUser } from "../../../test-helpers/helpers";
+import {
+  createUser,
+  DATABASE_CONSTRAINTS,
+} from "../../../test-helpers/helpers";
 import { API_URL_PREFIX } from "../../../src/config/env";
+import { dbConnection } from "../../../src/config/postgres";
 
 const ROUTE = `${API_URL_PREFIX}/users/me`;
 
-const maxUsernameLength = 16;
-const maxPasswordLength = 50;
-const maxDisplayName = 32;
+beforeAll(async () => {
+  httpServer.listen();
+});
+
+afterAll(async () => {
+  httpServer.close(async (err) => {
+    if (err) throw err;
+  });
+});
 
 describe(ROUTE, () => {
-  const username = faker.internet.username().substring(0, maxUsernameLength);
-  const password = faker.internet.password().substring(0, maxPasswordLength);
-  const email = faker.internet.email();
-  const profilePictureUrl = faker.system.filePath();
-  const displayName = faker.internet.displayName().substring(0, maxDisplayName);
+  const testUser = {
+    roleId: 2,
+    username: faker.internet
+      .username()
+      .substring(0, DATABASE_CONSTRAINTS.maxUsernameLength),
+    password: faker.internet
+      .password()
+      .substring(0, DATABASE_CONSTRAINTS.maxPasswordLength),
+    email: faker.internet.email(),
+    profilePictureUrl: faker.system.filePath(),
+    isDeleted: false,
+    isEmailConfirmed: true,
+    displayName: faker.internet
+      .username()
+      .substring(0, DATABASE_CONSTRAINTS.maxUsernameLength),
+  };
 
   describe(`DELETE - delete user account`, () => {
     describe("204", () => {
-      it("allows the user with role 'broadcaster' to delete his own account", async () => {
-        await createUser({
-          username,
-          password,
-          email,
-          roleId: 2,
-          isDeleted: false,
-          isEmailConfirmed: true,
-          displayName,
-          profilePictureUrl,
-        });
+      it("allows the user with role 'broadcaster' (role_id=2) to soft delete his own account", async () => {
+        await createUser(testUser);
         const agent = request.agent(httpServer);
         await agent
           .post(`${API_URL_PREFIX}/sessions`)
-          .send({ username, password })
+          .send({ username: testUser.username, password: testUser.password })
           .expect(200);
 
         await agent.delete(ROUTE).expect(204);
-      });
-    });
-  });
+        const pool = await dbConnection.open();
+        const dbResponse = await pool.query(
+          `SELECT * FROM appuser WHERE username = '${testUser.username}'`,
+        );
 
-  describe(`GET - get user account`, () => {
-    describe("200", () => {
-      it("responds with authenticated user object", async () => {
-        await createUser({
-          username,
-          password,
-          email,
-          roleId: 2,
-          isDeleted: false,
-          isEmailConfirmed: true,
-          displayName,
-          profilePictureUrl,
-        });
-        const agent = request.agent(httpServer);
-        await agent
-          .post(`${API_URL_PREFIX}/sessions`)
-          .send({ username, password })
-          .expect(200);
-
-        const response = await agent.get(ROUTE).expect(200);
-        expect(response.body).toStrictEqual({
-          results: {
-            email,
-            username,
-            userId: expect.any(Number),
-            uuid: expect.any(String),
-            permissions: {
-              own_audio_stream: ["create", "delete", "read", "update"],
-              any_broadcast: ["read"],
-              own_broadcast: ["create", "delete", "read", "update"],
-              own_user_account: ["create", "delete", "read", "update"],
-              own_chat_message: ["create", "delete", "read"],
-            },
-          },
-        });
+        expect(dbResponse.rows.length).toBe(1);
+        expect(dbResponse.rows[0].is_deleted).toBe(true);
       });
     });
   });
@@ -85,24 +65,15 @@ describe(ROUTE, () => {
   describe(`PATCH - update user account`, () => {
     describe("200", () => {
       it("responds with an updated user object", async () => {
-        await createUser({
-          username,
-          password,
-          email,
-          roleId: 2,
-          isDeleted: false,
-          isEmailConfirmed: true,
-          displayName,
-          profilePictureUrl,
-        });
+        await createUser(testUser);
         const agent = request.agent(httpServer);
         await agent
           .post(`${API_URL_PREFIX}/sessions`)
-          .send({ username, password })
+          .send({ username: testUser.username, password: testUser.password })
           .expect(200);
         const newUsername = faker.internet
           .username()
-          .substring(0, maxUsernameLength);
+          .substring(0, DATABASE_CONSTRAINTS.maxUsernameLength);
 
         const response = await agent
           .patch(ROUTE)
@@ -111,7 +82,7 @@ describe(ROUTE, () => {
 
         expect(response.body).toStrictEqual({
           results: {
-            email,
+            email: testUser.email,
             username: newUsername,
             userId: expect.any(Number),
             uuid: expect.any(String),
@@ -129,29 +100,17 @@ describe(ROUTE, () => {
 
     describe("409", () => {
       it("responds with an error if the user with the submited username already exists", async () => {
-        await createUser({
-          username,
-          password,
-          email,
-          roleId: 2,
-          isDeleted: false,
-          isEmailConfirmed: true,
-          displayName,
-          profilePictureUrl,
-        });
+        await createUser(testUser);
         const user2 = {
-          username: faker.internet.username().substring(0, maxUsernameLength),
-          password: faker.internet.password().substring(0, maxPasswordLength),
-        };
-        await createUser({
-          ...user2,
+          username: faker.internet
+            .username()
+            .substring(0, DATABASE_CONSTRAINTS.maxUsernameLength),
+          password: faker.internet
+            .password()
+            .substring(0, DATABASE_CONSTRAINTS.maxPasswordLength),
           email: faker.internet.email(),
-          roleId: 2,
-          isDeleted: false,
-          isEmailConfirmed: true,
-          displayName,
-          profilePictureUrl,
-        });
+        };
+        await createUser({ ...testUser, ...user2 });
         const agent = request.agent(httpServer);
         await agent
           .post(`${API_URL_PREFIX}/sessions`)
@@ -160,7 +119,7 @@ describe(ROUTE, () => {
 
         const response = await agent
           .patch(ROUTE)
-          .send({ username })
+          .send({ username: testUser.username })
           .expect(409);
 
         expect(response.body).toStrictEqual({
