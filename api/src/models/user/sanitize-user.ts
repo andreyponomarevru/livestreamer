810875af8ca04bd76d21df70ee8fc12dfaabced0
Andrey Ponomarev <info@andreyponomarev.ru>
2025-12-02 20:37:55 +0300

    Add more props

--- api/src/models/user/sanitize-user.ts
+++ api/src/models/user/sanitize-user.ts
@@ -3,10 +3,18 @@ import { SanitizedUser } from "../../types";
 
 export function sanitizeUser(user: User): SanitizedUser {
   return {
-    uuid: user.uuid || "",
+    uuid: user.uuid,
     userId: user.userId,
-    email: user.email || "",
+    email: user.email,
     username: user.username,
-    permissions: user.permissions || {},
+    permissions: user.permissions,
+    createdAt: user.createdAt,
+    lastLoginAt: user.lastLoginAt,
+    isEmailConfirmed: user.isEmailConfirmed,
+    displayName: user.displayName,
+    websiteUrl: user.websiteUrl,
+    about: user.about,
+    profilePictureUrl: user.profilePictureUrl,
+    subscriptionName: user.subscriptionName,
   };
 }
