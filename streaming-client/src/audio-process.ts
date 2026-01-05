2c3ae82e535ee3f24ea71b7fe616f1f16bac3340
Andrey Ponomarev <info@andreyponomarev.ru>
2026-01-05 11:55:47 +0300

    Refactor

--- streaming-client/src/audio-process.ts
+++ streaming-client/src/audio-process.ts
@@ -1,10 +1,17 @@
-import { spawn } from "child_process";
+import { spawn, type ChildProcessWithoutNullStreams } from "child_process";
 
 import { FFMPEG_ARGS } from "./config/audio";
 
-const child = spawn("ffmpeg", FFMPEG_ARGS);
-// Without piping to process.stderr, ffmpeg silently hangs
-// after a few minutes
-child.stderr.pipe(process.stderr);
+let child: ChildProcessWithoutNullStreams | undefined;
 
-export { child as audioStream };
+export function spawnStreamProcess() {
+  if (!child) {
+    child = spawn("ffmpeg", FFMPEG_ARGS);
+    // Without piping to process.stderr, ffmpeg silently hangs
+    // after a few minutes
+
+    child.stderr.pipe(process.stderr);
+  }
+
+  return child;
+}
