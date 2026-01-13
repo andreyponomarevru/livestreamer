80d0eada0bbe1e3cea8a9bf9346babd210304570
Andrey Ponomarev <info@andreyponomarev.ru>
2026-01-13 16:30:35 +0300

    Add logging; do not emit irrelevant data

--- api/src/services/stream/events.ts
+++ api/src/services/stream/events.ts
@@ -1,20 +1,34 @@
+import util from "util";
 import EventEmitter from "events";
-import { type Broadcast, type SavedBroadcastLike } from "../../types";
+
+import { logger } from "../../config/logger";
+import { type SavedBroadcastLike } from "../../types";
 
 export class StreamEmitter extends EventEmitter {
-  start(broadcast: Broadcast & { listenersCount: number | undefined }): void {
+  start(broadcast: {
+    userId: number;
+    broadcastId: number;
+    listenerPeakCount: number;
+    likeCount: number;
+  }): void {
+    logger.debug(`StreamEmitter ['start'] ${util.inspect(broadcast)}`);
     this.emit("start", broadcast);
   }
 
   end(broadcastId: number): void {
+    logger.debug(`StreamEmitter ['end'] ${util.inspect(broadcastId)}`);
     this.emit("end", broadcastId);
   }
 
   like(like: SavedBroadcastLike & { likedByUserUUID: string }): void {
+    logger.debug(`StreamEmitter ['like'] ${util.inspect(like)}`);
     this.emit("like", like);
   }
 
   newListenersPeak(newPeakCount: { broadcastId: number; count: number }): void {
+    logger.debug(
+      `StreamEmitter ['listeners_peak'] ${util.inspect(newPeakCount)}`,
+    );
     this.emit("listeners_peak", newPeakCount);
   }
 }
