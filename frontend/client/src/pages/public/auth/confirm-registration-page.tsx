a6243e2f92fa61ae4aef4b39661672c316b1975a
Andrey Ponomarev <info@andreyponomarev.ru>
2026-01-30 18:19:28 +0300

    Replace useFetch with RTK Query

--- frontend/client/src/pages/public/auth/confirm-registration-page.tsx
+++ frontend/client/src/pages/public/auth/confirm-registration-page.tsx
@@ -2,13 +2,14 @@ import * as React from "react";
 import { NavLink, useNavigate } from "react-router";
 
 import { useQuery } from "../../../hooks/use-query";
-import { API_ROOT_URL } from "../../../config/env";
 import { Loader } from "../../../features/ui/loader/loader-component";
-import { useFetch } from "../../../hooks/use-fetch";
 import { Help } from "../../../features/ui/help/help-component";
 import { PATHS } from "../../../config/constants";
 import { useAppSelector } from "../../../hooks/redux-ts-helpers";
-import { selectCurrentUserProfile } from "../../../features/auth";
+import {
+  selectCurrentUserProfile,
+  useSendConfirmationTokenMutation,
+} from "../../../features/auth";
 import { PiSmileySad, PiSmiley } from "../../../features/ui/icons";
 import { Box } from "../../../features/ui/box/box-component";
 
@@ -22,60 +23,47 @@ export function ConfirmRegistrationPage(): React.ReactElement {
   const query = useQuery();
   const token = query.get("token");
 
-  const { state: confirmSignUpResponse, fetchNow: sendTokenRequest } =
-    useFetch();
+  const [
+    sendConfirmationToken,
+    {
+      isError: isSendTokenError,
+      isLoading: isSendTokenLoading,
+      isSuccess: isSendTokenSuccess,
+    },
+  ] = useSendConfirmationTokenMutation();
   React.useEffect(() => {
-    if (token) {
-      console.log("[confirmSignUp] Sending confirmation request...");
+    if (token) sendConfirmationToken(token);
+  }, [token, sendConfirmationToken]);
 
-      sendTokenRequest(`${API_ROOT_URL}/verification?token=${token}`, {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          accept: "application/json",
-        },
-      });
-    }
-  }, [token]);
+  if (isSendTokenSuccess) {
+    return (
+      <Box>
+        <PiSmiley fill="var(--color_success)" className="icon-size-m" />
+        <div className="text-size-xl text-color-success">
+          <p>Done!</p>
+          <p>
+            Now, you can{" "}
+            <NavLink to={PATHS.signIn} className="link">
+              log in
+            </NavLink>
+            .
+          </p>
+        </div>
+      </Box>
+    );
+  }
 
-  const [isConfirmed, setIsConfirmed] = React.useState<boolean | null>(null);
-  React.useEffect(() => {
-    if (confirmSignUpResponse.response) {
-      setIsConfirmed(true);
-    } else if (confirmSignUpResponse.error) {
-      setIsConfirmed(false);
-    }
-  }, [confirmSignUpResponse]);
+  if (isSendTokenError) {
+    return (
+      <Box>
+        <PiSmileySad fill="var(--color_danger)" className="icon-size-m" />
+        <span className="text-size-xl text-color-warning">
+          Sorry, we couldn't verify your email
+        </span>
+        <Help />
+      </Box>
+    );
+  }
 
-  return (
-    <Box>
-      {isConfirmed === false && (
-        <>
-          <PiSmileySad fill="var(--color_danger)" className="icon-size-m" />
-          <span className="text-size-xl text-color-warning">
-            Sorry, we couldn't verify your email
-          </span>
-          <Help />
-        </>
-      )}
-
-      {isConfirmed === true && (
-        <>
-          <PiSmiley fill="var(--color_success)" className="icon-size-m" />
-          <div className="text-size-xl text-color-success">
-            <p>Done!</p>
-            <p>
-              Now, you can{" "}
-              <NavLink to={PATHS.signIn} className="link">
-                log in
-              </NavLink>
-              .
-            </p>
-          </div>
-        </>
-      )}
-
-      {confirmSignUpResponse.isLoading && <Loader />}
-    </Box>
-  );
+  return <Box>{isSendTokenLoading && <Loader />}</Box>;
 }
