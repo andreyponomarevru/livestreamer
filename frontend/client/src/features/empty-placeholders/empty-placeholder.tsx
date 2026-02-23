be5c1d0c1d41c3336d1f60941f54e27d04824213
Andrey Ponomarev <info@andreyponomarev.ru>
2026-02-23 19:14:35 +0300

    Delete comp

--- frontend/client/src/features/empty-placeholders/empty-placeholder.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./empty-placeholder.module.css";
-
-export function EmptyPlaceholder(props: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={styles["empty-placeholder"]}>{props.children}</div>;
-}
