b198be0ae2e954c52116c8beed0dc73d24c73788
Andrey Ponomarev <info@andreyponomarev.ru>
2026-02-23 19:11:52 +0300

    Delete comp

--- frontend/client/src/features/streams/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from "./create-stream-form";
-export * from "./header";
-export * from "./share-stream-modal";
-export * from "./stream-card";
-export * from "./subheader";
-export * from "./warning";
